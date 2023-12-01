e39b9cd75a9117b8e6e08e9adbacc7ab8842d5a9
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-12-01 11:22:14 +0100

    implement getEnv script

---
+++ getEnv.sh
@@ -0,0 +1,7 @@
+SECRET_NAME="blog_project"
+
+SECRETS=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
+ENV_DIR="$ROOT_DIR/.env"
+touch $ENV_DIR
+echo $SECRETS | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > $ENV_DIR
+echo "Secrets fetched and stored in $ENV_DIR"
